38d06b49cc355faf0303e6d5139b9ffe58187d62
giuliamari04 <giuliamari04@gmail.com>
2023-10-27 14:33:21 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const {createApp}=Vue
 const miaApp = createApp({
     data(){
         return{
-            message: 'prima app in vue',     
+            message: 'prima app in vue', 
+            img: 'img/cheeems.png',  
+            imgClass:'w-50',  
         }
     }
    
