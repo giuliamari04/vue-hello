5e890b0d3d10220a7d99b79edb14b34351c7086e
giuliamari04 <giuliamari04@gmail.com>
2023-10-27 14:50:06 +0200

    decorazioni

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const {createApp}=Vue
 const miaApp = createApp({
     data(){
         return{
-            message: 'prima app in vue', 
+            message: 'Shiba Inu Samurai', 
+            textClass:'text-light',
             img: 'img/cheeems.png',  
-            imgClass:'w-50',  
+            imgClass:'w-50 m-4 rounded-4',  
+            sfondo:'d-flex flex-column justify-content-center align-items-center align-content-center my-bg h-100vh',
         }
     }
    
